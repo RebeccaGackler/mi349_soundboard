7b1a5b5dc660249f84f367a4fc55844117949fc8
Rebecca Gackler <rcgrebecca@gmail.com>
2020-11-15 22:57:30 -0500

    JS edits part 1

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var buttonElementOcean = document.getElementById('oceansfx')
-buttonElementOcean.addEventListener('click', function (){
-    buttonElementOcean.play()
-})
+var buttonElementOcean = document.getElementById('oceansfx');
+buttonElementOcean.onclick = function (){
+    buttonElementOcean.play ()
+}
 
-var buttonElementSoda = document.getElementById('sodasfx')
-buttonElementSoda.addEventListener('click', function (){
+var buttonElementSoda = document.getElementById('sodasfx');
+buttonElementSoda.addEventListener('click', function () {
     buttonElementSoda.play()
-})
+});
 
-var buttonElementSplash = document.getElementById('splashsfx')
-buttonElementSplash.addEventListener('click', function (){
+var buttonElementSplash = document.getElementById('splashsfx');
+buttonElementSplash.addEventListener('click', function () {
     buttonElementSplash.play ()
-})
+});
