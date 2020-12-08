e67d80e4267cf621ef4c242212518268e7db9579
Rebecca Gackler <rcgrebecca@gmail.com>
2020-12-08 10:12:40 -0500

    Added some semicolons

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var buttonElementOcean = document.getElementById('oceansfx');
 buttonElementOcean.onclick = function (){
-    buttonElementOcean.play('ocean.wav')
+    buttonElementOcean.play('ocean.wav');
 }
 
 var buttonElementSoda = document.getElementById('sodasfx');
 buttonElementSoda.onclick = function () {
-    buttonElementSoda.play('soda.wav')
+    buttonElementSoda.play('soda.wav');
 };
 
 var buttonElementSplash = document.getElementById('splashsfx');
 buttonElementSplash.onclick = function () {
-    buttonElementSplash.play('splash.wav')
+    buttonElementSplash.play('splash.wav');
 };
